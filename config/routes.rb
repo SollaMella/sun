50ab2eedbe52611683e2c7d5ede4c1ee2f20d7a5
potapchukanatoliy <potapchukanatoliy@penguin>
2020-05-18 19:34:47 +0200

    Second

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,15 @@
 Rails.application.routes.draw do
+  devise_for :accounts
   resources :departments
   resources :accounts
-  resources :users
   resources :receipt_items
   resources :receipts
   resources :employees
   resources :shops
   resources :items
   resources :items
+
+  root to: 'accounts#index'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
--- db/schema.rb
+++ db/schema.rb
@@ -11,14 +11,22 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20200518165447) do
+ActiveRecord::Schema.define(version: 20200518171134) do
 
   create_table "accounts", force: :cascade do |t|
     t.integer  "employee_id"
     t.string   "password"
-    t.datetime "created_at",  null: false
-    t.datetime "updated_at",  null: false
+    t.datetime "created_at",                          null: false
+    t.datetime "updated_at",                          null: false
+    t.string   "email",                  default: "", null: false
+    t.string   "encrypted_password",     default: "", null: false
+    t.string   "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
   end
+
+  add_index "accounts", ["email"], name: "index_accounts_on_email", unique: true
+  add_index "accounts", ["reset_password_token"], name: "index_accounts_on_reset_password_token", unique: true
 
   create_table "departments", force: :cascade do |t|
     t.integer  "role"
@@ -69,6 +77,7 @@ ActiveRecord::Schema.define(version: 20200518165447) do
     t.string   "password"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
+    t.string   "email"
   end
 
 end
